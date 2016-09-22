9c369969db4f732ff4db10857f15e1d6c67833a3
Ash9876 <ashappdevpt@gmail.com>
2016-09-22 09:48:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 
-var content={
+var articleone={
   title:'Article one, me',
   heading:'article one',
   date:'sep 5,2016',
@@ -21,13 +21,47 @@ var content={
             </p>`
 };
 
+function createtemplate(data){
+    var title=data.title;
+    var date=data.date;
+    var heading=data.heading;
+    var content=data.content;
+    
+var htmltemplate=`
+<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta none="viewport" content="width-device-width,initial-scale=1"/>
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+        <div>
+            <a href='/'>home</a>
+        </div>
+        <hr/>
+        <h3> ${heading}</h3>
+        <div>
+            ${date}
+        </div>
+        <div>
+            ${content}
+        </div>
+        </div>
+    </body>
+</html>
 
+`;
+return htmltemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one',function(req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+   res.send(createtemplate(articleone));
 });
 
 app.get('/article-two',function(req,res){
