6c6cc83914cc4978ecfb9c1dd01257c899cfb4c6
Ash9876 <ashappdevpt@gmail.com>
2016-09-22 10:29:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,5 +3,5 @@ var element=document.getElementById('maintext');
 element.innerHTML="new value";
 var img=document.getElementById('madi');
 img.onclick=function(){
-    img.style.margin="100px";
+    img.style.margin-left="100px";
 };
