2b08efd7baca4649d043a9b1d22a053191e16b70
Ash9876 <ashappdevpt@gmail.com>
2016-09-22 10:27:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,7 @@
 console.log('Loaded!');
 var element=document.getElementById('maintext');
 element.innerHTML="new value";
+var img=document.getElementById('madi');
+img.onclick=function(){
+    img.style.margin="100px";
+};
