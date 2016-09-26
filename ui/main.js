69275c98e70b5e783966275cd0029f48edf58abb
Ash9876 <ashappdevpt@gmail.com>
2016-09-26 09:56:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,9 @@
-console.log('Loaded!');
-var element=document.getElementById('maintext');
-element.innerHTML="new value";
-var img=document.getElementById('madi');
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft+1;
-    img.style.marginLeft=marginLeft+'px';
-}
-img.onclick=function(){
-    var interval=setInterval(moveRight,50);
+var button=document.getElementById('counter');
+button.onclick=function(){
+    
+    
+    
+    counter=counter+1;
+    var span=document.getElementById('count');
+    span.innerHTML=count.toString();
 };
