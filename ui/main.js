f1a3c80d5e82783c23f1a21617f4a16f612cbf60
Ash9876 <ashappdevpt@gmail.com>
2016-09-26 10:18:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,18 @@
 var button=document.getElementById('counter');
-var counter=0;
 button.onclick=function(){
-    
-    
-    
-    counter=counter+1;
-    var span=document.getElementById('count');
-    span.innerHTML=counter.toString();
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        if(ready.readyState===XMLHttpRequest.DONE)
+        {
+            if(request.status===200)
+            {
+                var counter=request.responseText;
+                 var span=document.getElementById('count');
+                span.innerHTML=counter.toString();
+            }
+        }
+    };
+    request.open('GET','https://cloud.imad.hasura.io/cloud',true);
+    request.send(null);
+   
 };
