16362176f8ef094fdb579218b82a7782fb7d3b32
Ash9876 <ashappdevpt@gmail.com>
2016-11-19 11:06:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,9 +3,14 @@ var img1="http://i.ebayimg.com/images/g/bj8AAOxyXDhSgmRm/s-l300.jpg";
 var img2="https://s-media-cache-ak0.pinimg.com/236x/af/a5/a9/afa5a926dd91ff40eebfd62249056287.jpg";
 var img=document.getElementById("img");
 
-img.onclick=function(){
-   this.src=img2;
-};
+function change(){
+    if(img.src==img1)
+   img.src=img2;
+   else
+   img.src=img1;
+}
+
+setInterval(change,1000);
 
 var button=document.getElementById('counter');
 button.onclick=function(){
