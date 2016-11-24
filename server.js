f05af0579407a31eae88f45eaf3ab26f0e6cf5ce
Ash9876 <ashappdevpt@gmail.com>
2016-11-24 12:56:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.post('/my-db', function (req, res) {
    // var username=req.body.username;
    // var password=req.body.password;
     
-  pool.query('SELECT * FROM user WHERE username="ash"',function(err,result){
+  pool.query('SELECT * FROM user WHERE fullname="ashwathi"',function(err,result){
       if(err){
           res.status(500).send(err.toString());
       } else{
