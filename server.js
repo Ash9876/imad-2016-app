d104a44dc9ec72b2ada68eb035c6bfc3b2326d0a
Ash9876 <ashappdevpt@gmail.com>
2016-11-25 06:52:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
+var form = document.getElementById('form');
 
 app.post('/person-db', function (req, res) {
    var username=req.body.username;
