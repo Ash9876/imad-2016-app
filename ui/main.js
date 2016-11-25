530077c6d782d57e0232c5cc350900a817a07c48
Ash9876 <ashappdevpt@gmail.com>
2016-11-25 18:21:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -35,7 +35,7 @@ treat.onclick=function(){
         firstval.innerHTML=` I walked across the street <br>
                 So I could buy a treat
                 <br>`;
-                 template(input) ;
+                 var temp=template(input) ;
     };
     sheet.onclick=function(){
         
