f3da8dddab8ad10bae551f20e36acd5df521122c
Ash9876 <ashappdevpt@gmail.com>
2016-11-25 15:45:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -57,8 +57,8 @@ app.get('/signup.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'signup.html'));
 });
 
-app.get('/login.html', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'login.html'));
+app.get('/poem.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'poem.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
