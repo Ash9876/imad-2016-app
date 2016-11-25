9cc5fa8c1e0264ec17d70f1ec0cbc988c572dd78
Ash9876 <ashappdevpt@gmail.com>
2016-11-25 17:32:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,30 +20,11 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/person-db', function (req, res) {
-   var username=req.body.username;
-    var password=req.body.password;
-    
-  pool.query("SELECT * FROM person WHERE username=" + "'" + username + "'" ,function(err,result){
-      if(err){
-          res.status(500).send(err.toString());
-      } else{
-          res.send(JSON(stringify(result.rows)));
-      }
-  });
-});
-
-var bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-
-
 app.get('/person-db', function (req, res) {
    var username=req.body.username;
     var password=req.body.password;
     
-  pool.query("SELECT * FROM person",function(err,result){
+  pool.query("SELECT * FROM person WHERE username=" ,function(err,result){
       if(err){
           res.status(500).send(err.toString());
       } else{
