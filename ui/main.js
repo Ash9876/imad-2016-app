a3e7702382d680fd13bed0d6ded23af613c4bafb
Ash9876 <ashappdevpt@gmail.com>
2016-11-26 10:39:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -174,6 +174,226 @@ var inputs={
             you could leave before you pay<br>
             and so i went in for just one pastry<br>
             and went out paying for all of them!<br>`
+     },
+     input11:{
+        value1:'cat',
+        value2:'dog',
+        poem:`I walked across the street <br>
+            so I could buy a sheet<br>
+            of paper from the store<br>
+            to write a little poem<br>
+            i thought for long i did<br>
+            for something to write on<br>
+            and then i thought oh yes<br>
+            i would write about my <br>`
+     },
+     input12:{
+        value1:'bright sunny day',
+        value2:'nice rainy day',
+        poem:`I walked across the street <br>
+            so I could buy a sheet<br>
+            of paper from the store<br>
+            to write a little story<br>
+            about what i did <br>
+            on a`
+     },
+     input13:{
+        value1:null,
+        value2:null,
+        poem:`I walked across the street <br>
+            so I could buy a sheet<br>
+            of paper from the store<br>
+            to write a little story<br>
+            about what i did <br>
+            on a bright sunny day<br>
+            when the sun shone bright <br>
+            and the sky was clear<br>
+            but i didn't get the chance<br>
+            because all of a sudden <br>
+            the sky turned dark with clouds<br>
+            and the rain came pouring down<br>
+            so instead of a bright sunny day<br>
+            i got a nice rainy day<br>`
+     },
+     input14:{
+        value1:null,
+        value2:null,
+        poem:`I walked across the street <br>
+            so I could buy a sheet<br>
+            of paper from the store<br>
+            to write a little story<br>
+            about what i did <br>
+            on a nice rainy day<br>
+            when the rain drops danced <br>
+            and the ground cheered<br>
+            but i didn't get the chance<br>
+            because all of a sudden <br>
+            the sky cleared, the clouds gone<br>
+            and the sun shone down bright<br>
+            so instead of a nice rainy day<br>
+            i got a bright sunny day<br>`
+     },
+     input15:{
+        value1:'black and white',
+        value2:'fully white',
+        poem:`I walked across the street <br>
+            so I could buy a sheet<br>
+            of paper from the store<br>
+            to write a little poem<br>
+            i thought for long i did<br>
+            for something to write on<br>
+            and then i thought oh yes<br>
+            i would write about my cat<br>
+            my cat cute but sly<br>
+            shes coloured <br>`
+     },
+     input16:{
+        value1:'pounced',
+        value2:'scratched',
+        poem:`I walked across the street <br>
+            so I could buy a sheet<br>
+            of paper from the store<br>
+            to write a little poem<br>
+            i thought for long i did<br>
+            for something to write on<br>
+            and then i thought oh yes<br>
+            i would write about my cat<br>
+            my cat cute but sly<br>
+            shes coloured black and white<br>
+            like an old movie or photo<br>
+            i got myself the paper <br>
+            and went back home to write<br>
+            but just as i started <br>
+            my cat, oh yes, she<br>`
+     },
+     input17:{
+        value1:'pounced',
+        value2:'scratched',
+        poem:`I walked across the street <br>
+            so I could buy a sheet<br>
+            of paper from the store<br>
+            to write a little poem<br>
+            i thought for long i did<br>
+            for something to write on<br>
+            and then i thought oh yes<br>
+            i would write about my cat<br>
+            my cat cute but sly<br>
+            she's coloured fully white<br>
+            like she fell into white paint<br>
+            i got myself the paper <br>
+            and went back home to write<br>
+            but just as i started <br>
+            my cat, oh yes, she<br>`
+     },
+     input18:{
+        value1:null,
+        value2:null,
+        poem:`I walked across the street <br>
+            so I could buy a sheet<br>
+            of paper from the store<br>
+            to write a little poem<br>
+            i thought for long i did<br>
+            for something to write on<br>
+            and then i thought oh yes<br>
+            i would write about my cat<br>
+            my cat cute but sly<br>
+            shes coloured black and white<br>
+            like an old movie or photo<br>
+            i got myself the paper <br>
+            and went back home to write<br>
+            but just as i started <br>
+            my cat, oh yes, she pounced<br>
+            onto the sheet of paper<br>
+            filling it with her paw prints<br>
+            and so instead her poem<br>
+            i had her set of paw prints!<br>`
+     },
+     input19:{
+        value1:null,
+        value2:null,
+        poem:`I walked across the street <br>
+            so I could buy a sheet<br>
+            of paper from the store<br>
+            to write a little poem<br>
+            i thought for long i did<br>
+            for something to write on<br>
+            and then i thought oh yes<br>
+            i would write about my cat<br>
+            my cat cute but sly<br>
+            shes coloured black and white<br>
+            like an old movie or photo<br>
+            i got myself the paper <br>
+            and went back home to write<br>
+            but just as i started <br>
+            my cat, oh yes, she scratched<br>
+            all over the paper like mad<br>
+            the paper torn into shreds<br>
+            and so instead of a poem <br>
+            i had just shreds of paper<br>`
+     },
+     input20:{
+        value1:'barked',
+        value2:'growled',
+        poem:`I walked across the street <br>
+            so I could buy a sheet<br>
+            of paper from the store<br>
+            to write a little poem<br>
+            i thought for long i did<br>
+            for something to write on<br>
+            and then i thought oh yes<br>
+            i would write about my dog<br>
+            my dog nice but large<br>
+            shes coloured brown and white<br>
+            i got myself the paper <br>
+            and went back home to write<br>
+            but just as i started <br>
+            my dog, oh yes, she<br>`
+     },
+     input21:{
+        value1:null,
+        value2:null,
+        poem:`I walked across the street <br>
+            so I could buy a sheet<br>
+            of paper from the store<br>
+            to write a little poem<br>
+            i thought for long i did<br>
+            for something to write on<br>
+            and then i thought oh yes<br>
+            i would write about my dog<br>
+            my dog nice but large<br>
+            shes coloured brown and white<br>
+            i got myself the paper <br>
+            and went back home to write<br>
+            but just as i started <br>
+            my dog, oh yes, she barked<br>
+            and ran right into me<br>
+            i tripped and fell, face first<br>
+            the paper completely soiled<br>
+            and so instead her poem <br>
+            i had a sheet of soiled paper!<br>`
+     },
+     input22:{
+        value1:null,
+        value2:null,
+        poem:`I walked across the street <br>
+            so I could buy a sheet<br>
+            of paper from the store<br>
+            to write a little poem<br>
+            i thought for long i did<br>
+            for something to write on<br>
+            and then i thought oh yes<br>
+            i would write about my dog<br>
+            my dog nice but large<br>
+            shes coloured brown and white<br>
+            i got myself the paper <br>
+            and went back home to write<br>
+            but just as i started <br>
+            my dog, oh yes, she growled<br>
+            looking straight at me<br>
+            she lunged at me with speed<br>
+            and caught the sheet in her mouth<br>
+            and so instead her poem <br>
+            she had the sheet of paper!<br>`
      }
 };
 
@@ -269,4 +489,68 @@ option1.onclick=function(){
     
 option2.onclick=function(){
         firstval.innerHTML=template(inputs.input2);
+            var option11=document.getElementById(inputs.input2.value1);
+            option11.onclick=function(){
+            firstval.innerHTML=template(inputs.input11);
+                var option15=document.getElementById(inputs.input11.value1);
+                option15.onclick=function(){
+                firstval.innerHTML=template(inputs.input15);
+                    var option16=document.getElementById(inputs.input15.value1);
+                    option16.onclick=function(){
+                    firstval.innerHTML=template(inputs.input16);
+                        var option18=document.getElementById(inputs.input16.value1);
+                        option18.onclick=function(){
+                        firstval.innerHTML=last(inputs.input18);
+            
+                        };
+                        var option19=document.getElementById(inputs.input16.value2);
+                        option19.onclick=function(){
+                        firstval.innerHTML=last(inputs.input19);
+                
+                        };
+                    };
+                    var option17=document.getElementById(inputs.input15.value2);
+                    option17.onclick=function(){
+                    firstval.innerHTML=template(inputs.input17);
+                        var option18=document.getElementById(inputs.input17.value1);
+                        option18.onclick=function(){
+                        firstval.innerHTML=last(inputs.input18);
+            
+                        };
+                        var option19=document.getElementById(inputs.input17.value2);
+                        option19.onclick=function(){
+                        firstval.innerHTML=last(inputs.input19);
+                
+                        };
+                    };
+                };
+                var option20=document.getElementById(inputs.input11.value2);
+                option20.onclick=function(){
+                firstval.innerHTML=template(inputs.input20);
+                    var option21=document.getElementById(inputs.input20.value1);
+                    option21.onclick=function(){
+                    firstval.innerHTML=last(inputs.input21);
+                    var option22=document.getElementById(inputs.input20.value2);
+                    option22.onclick=function(){
+                    firstval.innerHTML=last(inputs.input22);
+                
+                };
+                
+                };
+                };
+                
+            };
+            var option12=document.getElementById(inputs.input2.value2);
+            option12.onclick=function(){
+            firstval.innerHTML=template(inputs.input12);
+                var option13=document.getElementById(inputs.input12.value1);
+                option13.onclick=function(){
+                firstval.innerHTML=last(inputs.input13);
+                };
+                var option14=document.getElementById(inputs.input12.value2);
+                option14.onclick=function(){
+                firstval.innerHTML=last(inputs.input14);
+                
+                };
+            };
     };
