510063c3ec593c7364c91a565f5948870eb6ec91
Ash9876 <ashappdevpt@gmail.com>
2016-11-26 08:53:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,20 +1,4 @@
 
-var img1="http://i.ebayimg.com/images/g/bj8AAOxyXDhSgmRm/s-l300.jpg";
-var img2="https://s-media-cache-ak0.pinimg.com/236x/af/a5/a9/afa5a926dd91ff40eebfd62249056287.jpg";
-var img=document.getElementById("img");
-
-
-function change(){
-    if(img.src==img1){
-   img.src=img2;
-    }
-   else
-   img.src=img1;
-}
-
-setInterval(change,1000);
-
-
 
 var inputs={
     input0:{
